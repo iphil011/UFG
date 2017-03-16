d635d03167e995d3cd9312eb9d1db992dd7c12f7
Abdul-Haseeb Malik <amali046@gold.ac.uk>
2017-03-16 11:12:20 +0000

    Some Hit Work

---
+++ UFG/UFG/Assets/Player2Hit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Hit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Attack1P2") { 
+			Vector3 up = new Vector2(0.5f, 0.5f);
+			transform.position += up;
+		}
+		if (collision.gameObject.tag == "Attack2P2")
+		{
+			Vector3 up = new Vector2(0.2f, 0.2f);
+			transform.position += up;
+		}
+		if (collision.gameObject.tag == "Attack3P2")
+		{
+			Vector3 up = new Vector2(1.5f, 1.5f);
+			transform.position += up;
+		}
+
+	}
+}
---
+++ UFG/UFG/Assets/dummyHit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dummyHit : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Attack1") { 
+            Vector3 up = new Vector2(0.5f, 0.5f);
+            transform.position += up;
+        }
+        if (collision.gameObject.tag == "Attack2")
+        {
+            Vector3 up = new Vector2(0.2f, 0.2f);
+            transform.position += up;
+        }
+        if (collision.gameObject.tag == "Attack3")
+        {
+            Vector3 up = new Vector2(1.5f, 1.5f);
+            transform.position += up;
+        }
+
+    }
+}
