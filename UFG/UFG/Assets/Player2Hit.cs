3251790ea094af2f43438dd8d9afd96fb04b06d7
polarbear23 <amali046@gold.ac.uk>
2017-03-20 17:19:18 +0000

    Player can hit character in both directions now

--- UFG/UFG/Assets/Player2Hit.cs
+++ UFG/UFG/Assets/Player2Hit.cs
@@ -2,34 +2,89 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player2Hit : MonoBehaviour {
+public class Player2Hit : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+    public bool flipped = false;
+    Transform myTrans;
+    public GameObject player2;
+    // Use this for initialization
+    void Start()
+    {
+        myTrans = GetComponent<Transform>();
 
-	}
+    }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
+        Flip();
 
-	}
+    }
+    public void Flip()
+    {
 
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-		if (collision.gameObject.tag == "Attack1P2") { 
-			Vector3 up = new Vector2(0.5f, 0.5f);
-			transform.position += up;
-		}
-		if (collision.gameObject.tag == "Attack2P2")
-		{
-			Vector3 up = new Vector2(0.2f, 0.2f);
-			transform.position += up;
-		}
-		if (collision.gameObject.tag == "Attack3P2")
-		{
-			Vector3 up = new Vector2(1.5f, 1.5f);
-			transform.position += up;
-		}
+        if (flipped == false)
+        {
+            if (myTrans.position.x - player2.transform.position.x > 1)
+            {
+                flipped = true;
+            }
+        }
+        else if (flipped == true)
+        {
+            if (myTrans.position.x - player2.transform.position.x < 0)
+            {
+                flipped = false;
+            }
+        }
+    }
 
-	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Attack1P2")
+        {
+            if (flipped == true)
+            {
+                Vector3 up = new Vector2(0.5f, 0.5f);
+                transform.position += up;
+            }
+            else if (flipped == false)
+            {
+                Vector3 up = new Vector2(-0.5f, 0.5f);
+                transform.position += up;
+
+
+            }
+        }
+        if (collision.gameObject.tag == "Attack2P2")
+        {
+            if (flipped == true)
+            {
+                Vector3 up = new Vector2(0.2f, 0.2f);
+                transform.position += up;
+            }
+            else if (flipped == false)
+            {
+                Vector3 up = new Vector2(-0.2f, 0.2f);
+                transform.position += up;
+            }
+        }
+        if (collision.gameObject.tag == "Attack3P2")
+        {
+            if (flipped == true)
+            {
+                Vector3 up = new Vector2(1.5f, 1.5f);
+                transform.position += up;
+            }
+            else if (flipped == false)
+            {
+                Vector3 up = new Vector2(-1.5f, 1.5f);
+                transform.position += up;
+            }
+        }
+
+    }
 }
+  
+		
--- UFG/UFG/Assets/dummyHit.cs
+++ UFG/UFG/Assets/dummyHit.cs
@@ -3,32 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class dummyHit : MonoBehaviour {
-    
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool flipped = false;
+    Transform myTrans;
+    public GameObject player2;
+    // Use this for initialization
+    void Start () {
+        myTrans = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Flip();
+           
+}
+    public void Flip()
+    {
+
+        if (flipped == false)
+        {
+            if (myTrans.position.x - player2.transform.position.x > 1)
+            {
+                flipped = true;
+            }
+        }
+        else if (flipped == true)
+        {
+            if (myTrans.position.x - player2.transform.position.x < 0)
+            {
+                flipped = false;
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Attack1") { 
-            Vector3 up = new Vector2(0.5f, 0.5f);
-            transform.position += up;
+        if (collision.gameObject.tag == "Attack1") {
+            if (flipped ==true)
+            {
+                Vector3 up = new Vector2(0.5f, 0.5f);
+                transform.position += up;
+            }
+            else if (flipped==false)
+            {
+                Vector3 up = new Vector2(-0.5f, 0.5f);
+                transform.position += up;
+
+
+            }
         }
         if (collision.gameObject.tag == "Attack2")
         {
-            Vector3 up = new Vector2(0.2f, 0.2f);
-            transform.position += up;
-        }
+            if (flipped == true)
+            {
+                Vector3 up = new Vector2(0.2f, 0.2f);
+                transform.position += up;
+            }
+            else if (flipped == false)
+            {
+                Vector3 up = new Vector2(-0.2f, 0.2f);
+                transform.position += up;
+            }
+            }
         if (collision.gameObject.tag == "Attack3")
         {
-            Vector3 up = new Vector2(1.5f, 1.5f);
-            transform.position += up;
+            if (flipped == true)
+            {
+                Vector3 up = new Vector2(1.5f, 1.5f);
+                transform.position += up;
+            }
+            else if (flipped == false)
+            {
+                Vector3 up = new Vector2(-1.5f, 1.5f);
+                transform.position += up;
+            }
         }
 
     }
