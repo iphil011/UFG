ecb54137d280b58570e84887189acf57671ced1f
OsmanAbdulMoomin <o.abdulmoomin@gmail.com>
2017-04-17 19:16:31 +0100

    Scene switch from main menu to char select

---
+++ UFG/Assets/SwitchToNextScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchToNextScene : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			
+			Application.LoadLevel (Application.loadedLevel + 1);
+		}
+	}
+}
