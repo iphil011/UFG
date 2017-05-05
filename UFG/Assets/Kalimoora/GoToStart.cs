853bca7be3a658c369c1b20c9fe497ad34832ca9
polarbear23 <amali046@gold.ac.uk>
2017-05-05 16:14:39 +0100

    fixed osmans script name

--- UFG/Assets/Kalimoora/GoToStart.cs
+++ UFG/Assets/Kalimoora/GoToStart.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoToStart : MonoBehaviour {
+public class s : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+	
 	
 	// Update is called once per frame
 	void Update () {
